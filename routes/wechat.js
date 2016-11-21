5cf1a40345f5f3b53dbef88e50fad0e3409c7339
‘hankliu’ <397694072@qq.com>
2016-11-21 21:37:50 +0800

    wechat-oauth: modify create jssdk signature timestamp type

--- routes/wechat.js
+++ routes/wechat.js
@@ -226,7 +226,7 @@ router.get('/get_jssdk_signature', function (req, res, next){
       var current = new Date();
       var ticketSrc = data.ticket;
       var noncestr = WechatUtil.createNonceStr();
-      var timestamp = parseInt(current.getTime() / 1000, 10) + '';
+      var timestamp = parseInt(current.getTime() / 1000, 10);
       var signature = WechatUtil.createSignature(ticketSrc, noncestr, timestamp, url);
       var newTicket = {
         appid: wechatConfig.appid,
