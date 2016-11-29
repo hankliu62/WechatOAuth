eb15c647820225efec53cae76661991e769a9c93
hankliu <hankliu@augmentum.com.cn>
2016-11-29 12:11:21 +0800

    oauth: add test aug webhook api

--- routes/agwebhook.js
+++ routes/agwebhook.js
@@ -7,7 +7,7 @@ router.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
+  res.header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
   next();
 });
 
