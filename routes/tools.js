83a4c286880748263ce0b6c9971615f6ad7619e2
‘hankliu’ <397694072@qq.com>
2016-12-23 00:10:29 +0800

    fix(tools) fix typo and modify Access-Control-Allow-Headers value

--- routes/tools.js
+++ routes/tools.js
@@ -13,7 +13,7 @@ var HEX_CONVERT_STRING_TYPES = CONSTANTS.ToolsModule.HEX_CONVERT_STRING_TYPES;
 //设置跨域访问
 router.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1')
   res.header("Content-Type", "application/json;charset=utf-8");
